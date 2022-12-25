08fa129365da65edbe4a1b57b7774141280c9ba0
Emma Liu <emmatliu@gmail.com>
2022-12-24 22:59:30 -0500

    Working?

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,30 +1,48 @@
+#!/usr/bin/env python3
+
 import sys
 import argparse
 
-# Function to find most frequent occurrence
-
-def mostCommon():
-    # Stuff here
-
-# Function to create map with key:value = date:most common cookie
-
-def parseCookies():
-    # Calls mostCommon
-
 # Function to parse csv in format required for parseCookies
 
-def parseCsv():
-    # Stuff here
+def parseCsv(filename, date):
+    countDict = {}
+
+    with open(filename) as file:
+        for line in file:
+            pair = line.split(',')
+
+            cookie = pair[0]
+            ts = pair[1][:pair[1].find('T')] if pair[1].find('T') != -1 else ""
+
+            if not ts or ts != date:
+                continue
+
+            if cookie in countDict:
+                countDict[cookie] += 1
+            else:
+                countDict[cookie] = 1
+
+    return [cookie for cookie, count in countDict.items() if count == max(countDict.values())]
 
 # Function to get command line arguments
 
-def parseArgs():
-    # Stuff here
+def parseArgs(inputArgs):
+    argParser = argparse.ArgumentParser()
+
+    argParser.add_argument("file") # file
+    argParser.add_argument("-d") # date
+
+    args = argParser.parse_args(inputArgs)
+
+    return args.file, args.d
 
 # Main function
 
 def main():
-    # date, file = parseArgs()
-    # timestamps = parseCsv(file)
-    # map = parseCookies(timestamps)
-    # return map[date]
+    filename, date = parseArgs(sys.argv[1:])
+    res = parseCsv(filename, date)
+    print('\n'.join(res))
+
+if __name__ == "__main__":
+    main()
