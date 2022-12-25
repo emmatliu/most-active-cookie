02e3df27409ba1101aaaecf5099881c927c35646
Emma Liu <emmatliu@gmail.com>
2022-12-24 22:17:26 -0500

    Setting skeleton code up

---
+++ most_active_cookie.py
@@ -0,0 +1,30 @@
+import sys
+import argparse
+
+# Function to find most frequent occurrence
+
+def mostCommon():
+    # Stuff here
+
+# Function to create map with key:value = date:most common cookie
+
+def parseCookies():
+    # Calls mostCommon
+
+# Function to parse csv in format required for parseCookies
+
+def parseCsv():
+    # Stuff here
+
+# Function to get command line arguments
+
+def parseArgs():
+    # Stuff here
+
+# Main function
+
+def main():
+    # date, file = parseArgs()
+    # timestamps = parseCsv(file)
+    # map = parseCookies(timestamps)
+    # return map[date]
